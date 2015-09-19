238a6ff5b23411e317831bad4b9ec79a47a93c42
GeorgH93 <git@georgh93.eu>
2015-09-20 00:36:02 +0200

    Add more functions Add JavaDoc

--- src/at/pcgamingfreaks/UUIDConverter.java
+++ src/at/pcgamingfreaks/UUIDConverter.java
@@ -20,15 +20,40 @@ package at.pcgamingfreaks;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.UUID;
 
 import com.google.common.base.Charsets;
+import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 
+/**
+ * Functions to get UUIDs to player names. This library doesn't cache the results! You will have to do this on your own!
+ * Works with Bukkit and BungeeCord!
+ */
 public class UUIDConverter
 {
+	/**
+	 * Gets the current name of the player from the Mojang servers.
+	 * Only works for Mojang-UUIDs, not for Bukkit-Offline-UUIDs.
+	 *
+	 * @param uuid The UUID of the player.
+	 * @return The name of the player.
+	 */
+	public static String getNameFromUUID(UUID uuid)
+	{
+		return getNameFromUUID(uuid.toString());
+	}
+
+	/**
+	 * Gets the current name of the player from the Mojang servers.
+	 * Only works for Mojang-UUIDs, not for Bukkit-Offline-UUIDs.
+	 *
+	 * @param uuid The UUID of the player.
+	 * @return The name of the player.
+	 */
 	public static String getNameFromUUID(String uuid)
 	{
 		String name = null;
@@ -36,8 +61,7 @@ public class UUIDConverter
 		{
 			URL url = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuid.replaceAll("-", ""));
 			Scanner jsonScanner = new Scanner(url.openConnection().getInputStream(), "UTF-8");
-			String json = jsonScanner.next();
-			name = (((JsonObject)new JsonParser().parse(json)).get("name")).toString();
+			name = (((JsonObject)new JsonParser().parse(jsonScanner.next())).get("name")).toString();
 			jsonScanner.close();
 		}
 		catch (Exception e)
@@ -46,47 +70,264 @@ public class UUIDConverter
 		}
 		return name;
 	}
-	
-	public static String getUUIDFromName(String name, boolean onlinemode)
+
+	/**
+	 * A helper class to store the name changes and dates
+	 */
+	public class NameChange
 	{
-		return getUUIDFromName(name, onlinemode, false);
+		/**
+		 * The name to witch the name was changed
+		 */
+		public String name;
+
+		/**
+		 * Datetime of the name change in UNIX time (without milliseconds)
+		 */
+		public long changedToAt;
+
+		/**
+		 * Gets the date of a name change
+		 *
+		 * @return Date of the name change
+		 */
+		public Date getChangeDate()
+		{
+			return new Date(changedToAt);
+		}
 	}
 
-	public static String getUUIDFromName(String name, boolean onlinemode, boolean withSeperators)
+	/**
+	 * Gets the name history of a player from the Mojang servers.
+	 * Only works for Mojang-UUIDs, not for Bukkit-Offline-UUIDs.
+	 *
+	 * @param uuid The UUID of the player.
+	 * @return The names and name change dates of the player.
+	 */
+	public static NameChange[] getNamesFromUUID(UUID uuid)
 	{
-		String uuid = null;
-		if(onlinemode)
+		return getNamesFromUUID(uuid.toString());
+	}
+
+	/**
+	 * Gets the name history of a player from the Mojang servers.
+	 * Only works for Mojang-UUIDs, not for Bukkit-Offline-UUIDs.
+	 *
+	 * @param uuid The UUID of the player.
+	 * @return The names and name change dates of the player.
+	 */
+	public static NameChange[] getNamesFromUUID(String uuid)
+	{
+		NameChange[] names = null;
+		try
 		{
-			try
+			URL url = new URL("https://api.mojang.com/user/profiles/" + uuid.replaceAll("-", "") + "/names");
+			Scanner jsonScanner = new Scanner(url.openConnection().getInputStream(), "UTF-8");
+			names = (new Gson()).fromJson(jsonScanner.next(), NameChange[].class);
+			jsonScanner.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return names;
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @return The requested UUID.
+	 */
+	public static String getUUIDFromName(String name, boolean onlineMode)
+	{
+		return getUUIDFromName(name, onlineMode, false, false, null);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param lastKnownDate The last time you know that the player had this name.
+	 * @return The requested UUID.
+	 */
+	public static String getUUIDFromName(String name, boolean onlineMode, Date lastKnownDate)
+	{
+		return getUUIDFromName(name, onlineMode, false, false, lastKnownDate);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param withSeparators True will return the UUID with '-' separators (format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).
+	 *                       False will return the UUID without the '-' separator (format: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx).
+	 * @return The requested UUID.
+	 */
+	public static String getUUIDFromName(String name, boolean onlineMode, boolean withSeparators)
+	{
+		return getUUIDFromName(name, onlineMode, withSeparators, false, null);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param withSeparators True will return the UUID with '-' separators (format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).
+	 *                       False will return the UUID without the '-' separator (format: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx).
+	 * @param lastKnownDate The last time you know that the player had this name.
+	 * @return The requested UUID.
+	 */
+	public static String getUUIDFromName(String name, boolean onlineMode, boolean withSeparators, Date lastKnownDate)
+	{
+		return getUUIDFromName(name, onlineMode, withSeparators, false, lastKnownDate);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param withSeparators True will return the UUID with '-' separators (format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).
+	 *                       False will return the UUID without the '-' separator (format: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx).
+	 * @param offlineUUIDonFail True if an offline UUID should be returned if the Mojang server can't resolve the name.
+	 *                          False if null should be returned if the Mojang server doesn't return an UUID.
+	 * @return The requested UUID.
+	 */
+	public static String getUUIDFromName(String name, boolean onlineMode, boolean withSeparators, boolean offlineUUIDonFail)
+	{
+		return getUUIDFromName(name, onlineMode, withSeparators, offlineUUIDonFail, null);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param withSeparators True will return the UUID with '-' separators (format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx).
+	 *                       False will return the UUID without the '-' separator (format: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx).
+	 * @param offlineUUIDonFail True if an offline UUID should be returned if the Mojang server can't resolve the name.
+	 *                          False if null should be returned if the Mojang server doesn't return an UUID.
+	 * @param lastKnownDate The last time you know that the player had this name.
+	 * @return The requested UUID.
+	 */
+	public static String getUUIDFromName(String name, boolean onlineMode, boolean withSeparators, boolean offlineUUIDonFail, Date lastKnownDate)
+	{
+		String uuid;
+		if(onlineMode)
+		{
+			uuid = getOnlineUUID(name, lastKnownDate);
+			if(uuid == null)
 	    	{
-	    		BufferedReader in = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/" + name).openStream()));
-	    		uuid = (((JsonObject)new JsonParser().parse(in)).get("id")).toString().replaceAll("\"", "");
-	    		in.close();
-	    	}
-	    	catch (Exception e)
-	    	{
-	    		System.out.println("Unable to get UUID for: " + name + "! Giving offline UUID!");
-	    		//e.printStackTrace();
-	    		uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8)).toString();
+			    if(offlineUUIDonFail)
+			    {
+				    System.out.println("Using offline uuid for '" + name + "'.");
+				    uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8)).toString();
+			    }
+			    else
+			    {
+				    return null;
+			    }
 	    	}
 		}
 		else
 		{
 			uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8)).toString();
 		}
-		if(uuid != null)
+		// Fixing the separators depending on setting.
+		if(withSeparators)
+        {
+            if(!uuid.contains("-"))
+            {
+                uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+            }
+        }
+        else
+        {
+	        if(uuid.contains("-"))
+	        {
+		        uuid = uuid.replaceAll("-", "");
+	        }
+        }
+		return uuid;
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @return The requested UUID.
+	 */
+	public static UUID getUUIDFromNameAsUUID(String name, boolean onlineMode)
+	{
+		return getUUIDFromNameAsUUID(name, onlineMode, false, null);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param lastKnownDate The last time you know that the player had this name.
+	 * @return The requested UUID.
+	 */
+	public static UUID getUUIDFromNameAsUUID(String name, boolean onlineMode, Date lastKnownDate)
+	{
+		return getUUIDFromNameAsUUID(name, onlineMode, false, lastKnownDate);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param offlineUUIDonFail True if an offline UUID should be returned if the Mojang server can't resolve the name.
+	 *                          False if null should be returned if the Mojang server doesn't return an UUID.
+	 * @return The requested UUID.
+	 */
+	public static UUID getUUIDFromNameAsUUID(String name, boolean onlineMode, boolean offlineUUIDonFail)
+	{
+		return getUUIDFromNameAsUUID(name, onlineMode, offlineUUIDonFail, null);
+	}
+
+	/**
+	 * @param name The name of the player you want the uuid from.
+	 * @param onlineMode True the UUID should be an online mode UUID (from Mojang). False if it should be an offline mode UUID (from Bukkit).
+	 * @param offlineUUIDonFail True if an offline UUID should be returned if the Mojang server can't resolve the name.
+	 *                          False if null should be returned if the Mojang server doesn't return an UUID.
+	 * @param lastKnownDate The last time you know that the player had this name.
+	 * @return The requested UUID.
+	 */
+	public static UUID getUUIDFromNameAsUUID(String name, boolean onlineMode, boolean offlineUUIDonFail, Date lastKnownDate)
+	{
+		UUID uuid = null;
+		if(onlineMode)
 		{
-			if(withSeperators)
-    		{
-    			if(!uuid.contains("-"))
-    			{
-    				return uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
-    			}
-    		}
-    		else
-    		{
-    			uuid = uuid.replaceAll("-", "");
-    		}
+			String sUUID = getOnlineUUID(name, lastKnownDate);
+			if(sUUID != null)
+			{
+				uuid = UUID.fromString(sUUID.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5"));
+			}
+			else if(offlineUUIDonFail)
+			{
+				uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8));
+			}
+		}
+		else
+		{
+			uuid = UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(Charsets.UTF_8));
+		}
+		return uuid;
+	}
+
+	private static String getOnlineUUID(String name, Date at)
+	{
+		String uuid = null;
+		try
+		{
+			BufferedReader in = new BufferedReader(new InputStreamReader(new URL("https://api.mojang.com/users/profiles/minecraft/" + name + ((at != null) ? "?at=" + at.getTime() : "")).openStream()));
+			uuid = (((JsonObject)new JsonParser().parse(in)).get("id")).toString().replaceAll("\"", "");
+			in.close();
+		}
+		catch (Exception e)
+		{
+			if(at == null)
+			{
+				System.out.println("Unable to get UUID for: " + name + "!");
+			}
+			else
+			{
+				System.out.println("Unable to get UUID for: " + name + " at " + at.getTime() + "! Trying without date!");
+				uuid = getOnlineUUID(name, null);
+			}
+			//e.printStackTrace();
 		}
 		return uuid;
 	}
