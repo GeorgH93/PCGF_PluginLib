6f03f19317fc5f9a0a7fe2f76c721d58bad2d450
GeorgH93 <git@georgh93.eu>
2016-11-01 21:37:34 +0100

    Add new update provider

---
+++ src/at/pcgamingfreaks/Updater/UpdateProviders/AlwaysUpdateProvider.java
@@ -0,0 +1,163 @@
+/*
+ *   Copyright (C) 2016 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks.Updater.UpdateProviders;
+
+import at.pcgamingfreaks.Updater.ReleaseType;
+import at.pcgamingfreaks.Updater.UpdateResult;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Logger;
+
+/**
+ * An update provider that returns an very high version so that the file gets downloaded every time.
+ * The file to download is specified in the constructor.
+ * This provider should only be used for preview builds that will always download the newest build from a ci server.
+ */
+public class AlwaysUpdateProvider implements UpdateProvider
+{
+	private URL downloadUrl = null;
+	private ReleaseType releaseType;
+
+	/**
+	 * @param url The url to the file that should be downloaded.
+	 */
+	public AlwaysUpdateProvider(String url)
+	{
+		this(url, ReleaseType.RELEASE);
+	}
+
+	/**
+	 * @param url         The url to the file that should be downloaded.
+	 * @param releaseType The release type.
+	 */
+	public AlwaysUpdateProvider(String url, ReleaseType releaseType)
+	{
+		this.releaseType = releaseType;
+		try
+		{
+			downloadUrl = new URL(url);
+		}
+		catch(MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public UpdateResult query(Logger logger)
+	{
+		return UpdateResult.SUCCESS;
+	}
+
+	@Override
+	public String getLatestVersion() throws NotSuccessfullyQueriedException
+	{
+		return Integer.MAX_VALUE + "." + Integer.MAX_VALUE;
+	}
+
+	@Override
+	public URL getLatestFileURL() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		return downloadUrl;
+	}
+
+	@Override
+	public String getLatestVersionFileName() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		throw new RequestTypeNotAvailableException("This provider does not provide the name of the latest file.");
+	}
+
+	@Override
+	public String getLatestName() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		throw new RequestTypeNotAvailableException("This provider does not provide the name of the latest version.");
+	}
+
+	@Override
+	public String getLatestMinecraftVersion() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		throw new RequestTypeNotAvailableException("This provider does not provide the minecraft version of the latest version.");
+	}
+
+	@Override
+	public ReleaseType getLatestReleaseType() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		return releaseType;
+	}
+
+	@Override
+	public String getLatestChecksum() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		throw new RequestTypeNotAvailableException("This provider does not provide the checksum for the latest file.");
+	}
+
+	@Override
+	public String getLatestChangelog() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		throw new RequestTypeNotAvailableException("This provider does not provide the changelog for the latest file.");
+	}
+
+	@Override
+	public UpdateFile[] getLatestDependencies() throws RequestTypeNotAvailableException, NotSuccessfullyQueriedException
+	{
+		throw new RequestTypeNotAvailableException("This provider does not provide dependencies for the latest file.");
+	}
+
+	@Override
+	public boolean provideDownloadURL()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean provideMinecraftVersion()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean provideChangelog()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean provideMD5Checksum()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean provideReleaseType()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean provideUpdateHistory()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean provideDependencies()
+	{
+		return false;
+	}
+}
---
+++ test/src/at/pcgamingfreaks/Updater/UpdateProviders/AlwaysUpdateProviderTest.java
@@ -0,0 +1,176 @@
+/*
+ *   Copyright (C) 2016 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks.Updater.UpdateProviders;
+
+import at.pcgamingfreaks.Updater.ReleaseType;
+import at.pcgamingfreaks.Updater.UpdateResult;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AlwaysUpdateProviderTest
+{
+	private static final String url = "http://test.com/download/test.jar";
+	private final AlwaysUpdateProvider provider = new AlwaysUpdateProvider(url);
+
+	@Test
+	public void query() throws Exception
+	{
+		assertEquals("The result should match", UpdateResult.SUCCESS, provider.query(null));
+	}
+
+	@Test
+	public void testGetLatestVersion() throws Exception
+	{
+		assertEquals("The version strings should match", Integer.MAX_VALUE + "." + Integer.MAX_VALUE, provider.getLatestVersion());
+	}
+
+	@Test
+	public void testGetLatestFileURL() throws Exception
+	{
+		assertEquals("The url should match", url, provider.getLatestFileURL().toString());
+	}
+
+	@Test
+	public void testGetLatestVersionFileName() throws Exception
+	{
+		try
+		{
+			provider.getLatestVersionFileName();
+		}
+		catch(RequestTypeNotAvailableException e)
+		{
+			assertEquals("The exception message should match", "This provider does not provide the name of the latest file.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetLatestName() throws Exception
+	{
+		try
+		{
+			provider.getLatestName();
+		}
+		catch(RequestTypeNotAvailableException e)
+		{
+			assertEquals("This provider does not provide the name of the latest version.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetLatestMinecraftVersion() throws Exception
+	{
+		try
+		{
+			provider.getLatestMinecraftVersion();
+		}
+		catch(RequestTypeNotAvailableException e)
+		{
+			assertEquals("This provider does not provide the minecraft version of the latest version.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetLatestReleaseType() throws Exception
+	{
+		assertEquals("The release type should match", ReleaseType.RELEASE, provider.getLatestReleaseType());
+		assertEquals("The release type should match", ReleaseType.BETA, new AlwaysUpdateProvider(url, ReleaseType.BETA).getLatestReleaseType());
+	}
+
+	@Test
+	public void testGetLatestChecksum() throws Exception
+	{
+		try
+		{
+			provider.getLatestChecksum();
+		}
+		catch(RequestTypeNotAvailableException e)
+		{
+			assertEquals("This provider does not provide the checksum for the latest file.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetLatestChangelog() throws Exception
+	{
+		try
+		{
+			provider.getLatestChangelog();
+		}
+		catch(RequestTypeNotAvailableException e)
+		{
+			assertEquals("This provider does not provide the changelog for the latest file.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testGetLatestDependencies() throws Exception
+	{
+		try
+		{
+			provider.getLatestDependencies();
+		}
+		catch(RequestTypeNotAvailableException e)
+		{
+			assertEquals("This provider does not provide dependencies for the latest file.", e.getMessage());
+		}
+	}
+
+	@Test
+	public void testProvideDownloadURL() throws Exception
+	{
+		assertTrue(provider.provideDownloadURL());
+	}
+
+	@Test
+	public void testProvideMinecraftVersion() throws Exception
+	{
+		assertFalse(provider.provideMinecraftVersion());
+	}
+
+	@Test
+	public void testProvideChangelog() throws Exception
+	{
+		assertFalse(provider.provideChangelog());
+	}
+
+	@Test
+	public void testProvideMD5Checksum() throws Exception
+	{
+		assertFalse(provider.provideMD5Checksum());
+	}
+
+	@Test
+	public void testProvideReleaseType() throws Exception
+	{
+		assertTrue(provider.provideReleaseType());
+	}
+
+	@Test
+	public void testProvideUpdateHistory() throws Exception
+	{
+		assertFalse(provider.provideUpdateHistory());
+	}
+
+	@Test
+	public void testProvideDependencies() throws Exception
+	{
+		assertFalse(provider.provideDependencies());
+	}
+}
