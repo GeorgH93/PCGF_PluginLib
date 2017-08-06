9c5876968128698cc65eff6e41183d991747de5d
GeorgH93 <git@georgh93.eu>
2017-08-06 19:51:52 +0200

    Add simple time span calculator

---
+++ src/at/pcgamingfreaks/DateUtils.java
@@ -0,0 +1,141 @@
+/*
+ *   Copyright (C) 2017 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateUtils
+{
+	public static final byte YEAR = 0, MONTH = 1, DAY = 2, HOUR = 3, MINUTE = 4, SECOND = 5;
+	private static final int[] types = new int[] { Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH };
+
+	public static int[] timeSpan(long time)
+	{
+		return timeSpan(time, false);
+	}
+
+	public static int[] timeSpan(long time, boolean fastMode)
+	{
+		return timeSpan(time, System.currentTimeMillis(), fastMode);
+	}
+
+	public static int[] timeSpan(long timeFrom, long timeTo)
+	{
+		return timeSpan(timeFrom, timeTo, false);
+	}
+
+	public static int[] timeSpan(long timeFrom, long timeTo, boolean fastMode)
+	{
+		return timeSpan(timeFrom, null, timeTo, null, fastMode);
+	}
+
+	public static int[] timeSpan(Date time)
+	{
+		return timeSpan(time, false);
+	}
+
+	public static int[] timeSpan(Date time, boolean fastMode)
+	{
+		return timeSpan(time, new Date(), fastMode);
+	}
+
+	public static int[] timeSpan(Date timeFrom, Date timeTo)
+	{
+		return timeSpan(timeFrom, timeTo, false);
+	}
+
+	public static int[] timeSpan(Date timeFrom, Date timeTo, boolean fastMode)
+	{
+		return timeSpan(timeFrom.getTime(), null, timeTo.getTime(), null, fastMode);
+	}
+
+	public static int[] timeSpan(Calendar time)
+	{
+		return timeSpan(time, false);
+	}
+
+	public static int[] timeSpan(Calendar time, boolean fastMode)
+	{
+		return timeSpan(time, Calendar.getInstance(), fastMode);
+	}
+
+	public static int[] timeSpan(Calendar timeFrom, Calendar timeTo)
+	{
+		return timeSpan(timeFrom, timeTo, false);
+	}
+
+	public static int[] timeSpan(Calendar timeFrom, Calendar timeTo, boolean fastMode)
+	{
+		return timeSpan(timeFrom.getTimeInMillis(), (Calendar) timeFrom.clone(), timeTo.getTimeInMillis(), timeTo, fastMode);
+	}
+
+	//region calculation methods
+	@SuppressWarnings("MagicConstant")
+	private static int[] timeSpan(long from, @Nullable Calendar fromDate, long to, @Nullable Calendar toDate, boolean fastMode)
+	{
+		long totalDiffSeconds = ((to > from) ? (to - from) : (from - to)) / 1000L;
+		int[] diff = new int[] { -1, -1, -1, 0, 0, 0 };
+		if(fastMode)
+		{
+			diff[DAY] = (int) ((totalDiffSeconds) / 86400L);
+			diff[YEAR] = diff[DAY] / 365;
+			diff[DAY] -= diff[YEAR] * 365;
+			diff[MONTH] = (int)(diff[DAY] / (365/12.0));
+			diff[DAY] -= diff[MONTH] * (365/12.0);
+		}
+		else
+		{
+			if(fromDate == null)
+			{
+				fromDate = Calendar.getInstance();
+				fromDate.setTimeInMillis(from);
+			}
+			if(toDate == null)
+			{
+				toDate = Calendar.getInstance();
+				toDate.setTimeInMillis(to);
+			}
+
+			boolean future = toDate.after(fromDate);
+			for(int i = 0, dayOfMonth = fromDate.get(Calendar.DAY_OF_MONTH); i < types.length; i++)
+			{
+				while((future && !fromDate.after(toDate)) || (!future && !fromDate.before(toDate)))
+				{
+					fromDate.add(types[i], future ? 1 : -1);
+					if(types[i] != Calendar.DAY_OF_MONTH && fromDate.get(Calendar.DAY_OF_MONTH) < dayOfMonth && fromDate.get(Calendar.DAY_OF_MONTH) < fromDate.getActualMaximum(Calendar.DAY_OF_MONTH))
+					{
+						fromDate.set(Calendar.DAY_OF_MONTH, Math.min(fromDate.getActualMaximum(Calendar.DAY_OF_MONTH), dayOfMonth));
+					}
+					diff[i]++;
+				}
+				fromDate.add(types[i], future ? -1 : 1);
+			}
+		}
+
+		int secondsOnDay = (int) (totalDiffSeconds % 86400);
+		diff[HOUR] = secondsOnDay / 3600;
+		diff[MINUTE] = (secondsOnDay / 60) % 60;
+		diff[SECOND] = secondsOnDay % 60;
+
+		return diff;
+	}
+	//endregion
+}
---
+++ test/src/at/pcgamingfreaks/DateUtilsTest.java
@@ -0,0 +1,83 @@
+/*
+ *   Copyright (C) 2017 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks;
+
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class DateUtilsTest
+{
+	private static final long TIME1 = 1501948524000L, TIME2 = 1470412524000L, TIME3 = TIME2 + 1000L;
+
+	@Test
+	public void testTimeSpanLong()
+	{
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(TIME1, TIME2));
+		assertArrayEquals(new int[] {0,11,30,23,59,59}, DateUtils.timeSpan(TIME1, TIME3));
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(TIME2, TIME1));
+		assertArrayEquals(new int[] {0,11,30,23,59,59}, DateUtils.timeSpan(TIME3, TIME1));
+		assertArrayEquals(new int[] {0,0,0,0,0,1}, DateUtils.timeSpan(System.currentTimeMillis() - 1000L));
+		assertArrayEquals(new int[] {0,0,0,0,0,1}, DateUtils.timeSpan(System.currentTimeMillis() - 1000L, true));
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(TIME1, TIME2, true));
+		assertArrayEquals(new int[] {0,11,29,23,59,59}, DateUtils.timeSpan(TIME1, TIME3, true));
+	}
+
+	@Test
+	public void testTimeSpanDate()
+	{
+		Date date1 = new Date(TIME1), date2 = new Date(TIME2), date3 = new Date(TIME3);
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(date1, date2));
+		assertArrayEquals(new int[] {0,11,30,23,59,59}, DateUtils.timeSpan(date1, date3));
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(date2, date1));
+		assertArrayEquals(new int[] {0,11,30,23,59,59}, DateUtils.timeSpan(date3, date1));
+		assertArrayEquals(new int[] {0,0,0,0,0,1}, DateUtils.timeSpan(new Date(System.currentTimeMillis() - 1000L)));
+		assertArrayEquals(new int[] {0,0,0,0,0,1}, DateUtils.timeSpan(new Date(System.currentTimeMillis() - 1000L), true));
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(date1, date2, true));
+		assertArrayEquals(new int[] {0,11,29,23,59,59}, DateUtils.timeSpan(date1, date3, true));
+	}
+
+	@Test
+	public void testTimeSpanCalendar()
+	{
+		Calendar cal1 = Calendar.getInstance(), cal2 = Calendar.getInstance(), cal3 = Calendar.getInstance(), calNow = Calendar.getInstance();
+		cal1.setTimeInMillis(TIME1);
+		cal2.setTimeInMillis(TIME2);
+		cal3.setTimeInMillis(TIME3);
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(cal1, cal2));
+		assertArrayEquals(new int[] {0,11,30,23,59,59}, DateUtils.timeSpan(cal1, cal3));
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(cal2, cal1));
+		assertArrayEquals(new int[] {0,11,30,23,59,59}, DateUtils.timeSpan(cal3, cal1));
+		calNow.setTimeInMillis(System.currentTimeMillis() - 1000L);
+		assertArrayEquals(new int[] {0,0,0,0,0,1}, DateUtils.timeSpan(calNow));
+		assertArrayEquals(new int[] {0,0,0,0,0,1}, DateUtils.timeSpan(calNow, true));
+		assertArrayEquals(new int[] {1,0,0,0,0,0}, DateUtils.timeSpan(cal1, cal2, true));
+		assertArrayEquals(new int[] {0,11,29,23,59,59}, DateUtils.timeSpan(cal1, cal3, true));
+		cal1.set(Calendar.YEAR, 2017);
+		cal1.set(Calendar.MONTH, Calendar.JANUARY);
+		cal1.set(Calendar.DAY_OF_MONTH, 31);
+		cal2.set(Calendar.YEAR, 2017);
+		cal2.set(Calendar.MONTH, Calendar.MARCH);
+		cal2.set(Calendar.DAY_OF_MONTH, 1);
+		assertArrayEquals(new int[] {0,1,1,0,0,0}, DateUtils.timeSpan(cal1, cal2));
+		assertArrayEquals(new int[] {0,0,28 + 1,0,0,0}, DateUtils.timeSpan(cal1, cal2, true));
+	}
+}
