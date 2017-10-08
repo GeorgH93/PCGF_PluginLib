f0f0fd9e47a10b07e1e59d529935e42e12eb96e4
GeorgH93 <git@georgh93.eu>
2017-10-08 23:48:39 +0200

    Add translation for TimeSpan class

--- src/at/pcgamingfreaks/PluginLib/Bukkit/PluginLib.java
+++ src/at/pcgamingfreaks/PluginLib/Bukkit/PluginLib.java
@@ -17,12 +17,15 @@
 
 package at.pcgamingfreaks.PluginLib.Bukkit;
 
+import at.pcgamingfreaks.Bukkit.Language;
 import at.pcgamingfreaks.Bukkit.MCVersion;
 import at.pcgamingfreaks.Bukkit.Updater;
+import at.pcgamingfreaks.Calendar.TimeSpan;
 import at.pcgamingfreaks.ConsoleColor;
 import at.pcgamingfreaks.PluginLib.Database.DatabaseConnectionPool;
 import at.pcgamingfreaks.PluginLib.Database.DatabaseConnectionPoolBase;
 import at.pcgamingfreaks.PluginLib.PluginLibrary;
+import at.pcgamingfreaks.Reflection;
 import at.pcgamingfreaks.StringUtils;
 import at.pcgamingfreaks.Updater.UpdateProviders.AlwaysUpdateProvider;
 import at.pcgamingfreaks.Version;
@@ -31,6 +34,8 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.io.File;
 
 public final class PluginLib extends JavaPlugin implements PluginLibrary
 {
@@ -68,6 +73,20 @@ public final class PluginLib extends JavaPlugin implements PluginLibrary
 		}
 
 		new ItemNameResolver(this);
+
+		Language commonLanguage = new Language(this, 1, 1, File.separator + "lang", "common_");
+		String[] unitNames = new String[] { commonLanguage.get("Date.Units.Year"), commonLanguage.get("Date.Units.Years"), commonLanguage.get("Date.Units.Month"), commonLanguage.get("Date.Units.Months"),
+		                                    commonLanguage.get("Date.Units.Day"), commonLanguage.get("Date.Units.Days"), commonLanguage.get("Date.Units.Hour"), commonLanguage.get("Date.Units.Hours"),
+		                                    commonLanguage.get("Date.Units.Minute"), commonLanguage.get("Date.Units.Minutes"), commonLanguage.get("Date.Units.Second"), commonLanguage.get("Date.Units.Seconds") };
+		try
+		{
+			//noinspection ConstantConditions
+			Reflection.getField(TimeSpan.class, "timeUnitNames").set(null, unitNames);
+		}
+		catch(IllegalAccessException e)
+		{
+			e.printStackTrace();
+		}
 
 		instance = this;
 		this.getLogger().info(StringUtils.getPluginEnabledMessage(this.getDescription().getFullName()));
