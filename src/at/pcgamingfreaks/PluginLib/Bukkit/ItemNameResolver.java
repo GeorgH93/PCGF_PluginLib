8358923dc9ebcfa60a9cc4c9a94c9c77c0bf1c0d
GeorgH93 <git@georgh93.eu>
2018-05-08 20:15:29 +0200

    Generalise ItemNameResolver

--- src/at/pcgamingfreaks/PluginLib/Bukkit/ItemNameResolver.java
+++ src/at/pcgamingfreaks/PluginLib/Bukkit/ItemNameResolver.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2017 GeorgH93
+ *   Copyright (C) 2017, 2018 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -18,25 +18,18 @@
 package at.pcgamingfreaks.PluginLib.Bukkit;
 
 import at.pcgamingfreaks.Bukkit.Language;
-import at.pcgamingfreaks.Bukkit.MinecraftMaterial;
 
-import org.apache.commons.lang3.Validate;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This class allows to translate minecraft items names.
  */
-public final class ItemNameResolver
+public final class ItemNameResolver extends at.pcgamingfreaks.Bukkit.ItemNameResolver
 {
 	//region static stuff
-	private static ItemNameResolver instance = new ItemNameResolver();
+	private static ItemNameResolver instance = null;
 
 	public static ItemNameResolver getInstance()
 	{
@@ -44,146 +37,12 @@ public final class ItemNameResolver
 	}
 	//endregion
 
-	private final Map<Material, Map<Short, String>> names = new HashMap<>();
-	private final Language langReader;
-
-	private ItemNameResolver()
-	{
-		langReader = null;
-	}
-
 	/**
 	 * Packet local so that only the lib itself can create an instance.
 	 */
 	ItemNameResolver(@NotNull PluginLib plugin)
 	{
-		plugin.getLogger().info("Loading item translations ...");
-		langReader = new Language(plugin, -1, File.separator + "lang", "items_");
-		langReader.load(plugin.getConfiguration().getLanguage(), plugin.getConfiguration().getItemLangUpdateMode());
-		int translationCount = load();
-		plugin.getLogger().info("Finished loading item translations for " + translationCount + " items.");
+		super(new Language(plugin, -1, File.separator + "lang", "items_"), plugin.getLogger());
 		instance = this;
-	}
-
-	private int load()
-	{
-		int translationCount = 0;
-		for(String key : langReader.getLang().getKeys(true))
-		{
-			String material = key, suffix = "";
-			short dataValue = -1;
-			if(key.contains("."))
-			{
-				String[] components = key.split("\\.");
-				material = components[0];
-				if(components[1].equals("appendDefault")) continue;
-				try
-				{
-					dataValue = Short.parseShort(components[1]);
-				}
-				catch(NumberFormatException ignored) {}
-				if(langReader.getLang().getBoolean(material + ".appendDefault", false))
-				{
-					suffix = langReader.getLang().getString(material, "");
-				}
-			}
-			if(material.contains(":"))
-			{
-				String[] components = material.split(":");
-				material = components[0];
-				if(components[1].equals("appendDefault")) continue;
-				try
-				{
-					dataValue = Short.parseShort(components[1]);
-				}
-				catch(NumberFormatException ignored) {}
-				if(langReader.getLang().getBoolean(material + ".appendDefault", false))
-				{
-					suffix = langReader.getLang().getString(material, langReader.getLang().getString(material + ".default", ""));
-				}
-			}
-			Material mat = Material.matchMaterial(material);
-			if(!names.containsKey(mat))
-			{
-				names.put(mat, new HashMap<Short, String>());
-			}
-			names.get(mat).put(dataValue, langReader.get(key) + suffix);
-			translationCount++;
-		}
-		return translationCount;
-	}
-
-	/**
-	 * Gets the translated name for a material.
-	 *
-	 * @param material  The material to get the name for.
-	 * @return The translated material name.
-	 */
-	public String getName(@NotNull Material material)
-	{
-		return getName(material, (short) -1);
-	}
-
-	/**
-	 * Gets the translated name for a material with a data-value.
-	 *
-	 * @param material  The material to get the name for.
-	 * @param dataValue The data-value for sub materials.
-	 * @return The translated material name.
-	 */
-	public String getName(@NotNull Material material, short dataValue)
-	{
-		Validate.notNull(material);
-		Map<Short, String> namesForMaterial = names.get(material);
-		if(namesForMaterial != null)
-		{
-			if(dataValue >= -1 && namesForMaterial.containsKey(dataValue))
-			{
-				return namesForMaterial.get(dataValue);
-			}
-			if(dataValue != -1 && namesForMaterial.containsKey((short) -1))
-			{
-				return namesForMaterial.get((short) -1);
-			}
-		}
-		return material.name().toLowerCase();
-	}
-
-	/**
-	 * Gets the translated name for a minecraft material.
-	 *
-	 * @param material  The material to get the name for.
-	 * @return The translated material name.
-	 */
-	public String getName(@NotNull MinecraftMaterial material)
-	{
-		Validate.notNull(material);
-		return getName(material.getMaterial(), material.getDataValue());
-	}
-
-	/**
-	 * Gets the translated name for a block.
-	 *
-	 * @param block The block to get the name for.
-	 * @return The translated block material name.
-	 */
-	public String getName(@NotNull Block block)
-	{
-		Validate.notNull(block);
-		//noinspection deprecation
-		return getName(block.getType(), block.getData());
-	}
-
-	/**
-	 * Gets the translated name for an item-stack.
-	 *
-	 * @param itemStack The item-stack to get the name for.
-	 * @return The translated item-stack material name.
-	 */
-	public String getName(@NotNull ItemStack itemStack)
-	{
-		Validate.notNull(itemStack);
-		//noinspection deprecation
-		return getName(itemStack.getType(), itemStack.getDurability());
 	}
 }
