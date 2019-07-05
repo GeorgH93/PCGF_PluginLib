a1023f27301f0b669a7b1f26fbd956e92b4d37fb
GeorgH93 <git@georgh93.eu>
2019-07-05 19:11:20 +0200

    Fix using wrong reflection class

--- src/at/pcgamingfreaks/Bungee/Message/Sender/SendMethod.java
+++ src/at/pcgamingfreaks/Bungee/Message/Sender/SendMethod.java
@@ -1,23 +1,23 @@
 /*
- * Copyright (C) 2016 GeorgH93
+ *   Copyright (C) 2019 GeorgH93
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package at.pcgamingfreaks.Bungee.Message.Sender;
 
-import at.pcgamingfreaks.Bukkit.NMSReflection;
+import at.pcgamingfreaks.Reflection;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -40,7 +40,7 @@ public enum SendMethod
 	{
 		this.defaultSender = defaultSender;
 		this.metadataClass = metadataClass;
-		this.fromJsonMethod = (metadataClass != null) ? NMSReflection.getMethod(metadataClass, "fromJson", String.class) : null;
+		this.fromJsonMethod = (metadataClass != null) ? Reflection.getMethod(metadataClass, "fromJson", String.class) : null;
 	}
 
 	/**
