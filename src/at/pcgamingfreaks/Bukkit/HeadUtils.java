631f54c95bc154627f295b2b7480e6dfccd90a26
GeorgH93 <git@georgh93.eu>
2020-01-02 14:01:41 +0100

    Add PlayerHead API

---
+++ src/at/pcgamingfreaks/Bukkit/HeadUtils.java
@@ -0,0 +1,80 @@
+/*
+ *   Copyright (C) 2020 GeorgH93
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package at.pcgamingfreaks.Bukkit;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Base64;
+import java.util.UUID;
+
+public class HeadUtils
+{
+	private static final Material HEAD_MATERIAL = MCVersion.isNewerOrEqualThan(MCVersion.MC_1_13) ? Material.valueOf("PLAYER_HEAD") : Material.valueOf("SKULL");
+
+	public static ItemStack createHeadItemStack()
+	{
+		return createHeadItemStack(1);
+	}
+
+	public static ItemStack createHeadItemStack(int amount)
+	{
+		return (MCVersion.isNewerOrEqualThan(MCVersion.MC_1_13)) ? new ItemStack(HEAD_MATERIAL, amount) : new ItemStack(HEAD_MATERIAL, amount, (byte) 3);
+	}
+
+	public static ItemStack fromUrl(@NotNull String url, @Nullable String itemName, @Nullable UUID ownerUUID)
+	{
+		return fromBase64(encodeUrl(url), itemName, ownerUUID);
+	}
+
+	public static ItemStack fromUrl(@NotNull ItemStack item, @NotNull String url, @Nullable String itemName, @Nullable UUID ownerUUID)
+	{
+		return fromBase64(item, encodeUrl(url), itemName, ownerUUID);
+	}
+
+	public static ItemStack fromBase64(@NotNull String value, @Nullable String itemName, @Nullable UUID ownerUUID)
+	{
+		return fromBase64(createHeadItemStack(), value, itemName, ownerUUID);
+	}
+
+	public static ItemStack fromBase64(@NotNull ItemStack item, @NotNull String value, @Nullable String itemName, @Nullable UUID ownerUUID)
+	{
+		String name = "";
+		if(itemName != null)
+		{
+			name = (MCVersion.isNewerOrEqualThan(MCVersion.MC_1_13)) ? "{\\\"text\\\":\\\"" + itemName + "\\\"}" : itemName;
+			name = "display:{Name:\"" + name + "\"},";
+		}
+		if(ownerUUID == null)
+		{
+			ownerUUID = UUID.randomUUID();
+		}
+		String str = "{" + name + "SkullOwner:{Id:\"" + ownerUUID + "\",Properties:{textures:[{Value:\"" + value + "\"}]}}}";
+		Bukkit.getUnsafe().modifyItemStack(item, str);
+		return item;
+	}
+
+	private static String encodeUrl(String url)
+	{
+		String toEncode = "{\"textures\":{\"SKIN\":{\"url\":\"" + url + "\"}}}";
+		return Base64.getEncoder().encodeToString(toEncode.getBytes());
+	}
+}
