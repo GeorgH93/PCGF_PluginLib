46363192a9723b8a67f9653a8894fa0bc5fb9b6a
GeorgH93 <git@georgh93.eu>
2020-06-25 17:44:04 +0200

    Enable NBTItemStackSerializerGen2 on MC 1.16

--- src/at/pcgamingfreaks/Bukkit/ItemStackSerializer/NBTItemStackSerializerGen2.java
+++ src/at/pcgamingfreaks/Bukkit/ItemStackSerializer/NBTItemStackSerializerGen2.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2019 GeorgH93
+ *   Copyright (C) 2020 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -278,6 +278,6 @@ public class NBTItemStackSerializerGen2 implements ItemStackSerializer
 
 	public static boolean isMCVersionCompatible()
 	{
-		return MCVersion.isNewerOrEqualThan(MCVersion.MC_1_7) && MCVersion.isOlderOrEqualThan(MCVersion.MC_NMS_1_15_R1);
+		return MCVersion.isNewerOrEqualThan(MCVersion.MC_1_7) && MCVersion.isOlderOrEqualThan(MCVersion.MC_NMS_1_16_R1);
 	}
 }
