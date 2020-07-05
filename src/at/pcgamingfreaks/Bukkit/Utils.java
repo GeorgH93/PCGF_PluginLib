8f4dcd73bf703219542e74b878118c544020fe55
GeorgH93 <git@georgh93.eu>
2020-07-05 23:49:03 +0200

    Add updateInventoryTitle method

--- src/at/pcgamingfreaks/Bukkit/Utils.java
+++ src/at/pcgamingfreaks/Bukkit/Utils.java
@@ -27,13 +27,16 @@ import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -362,5 +365,40 @@ public class Utils extends at.pcgamingfreaks.Utils
 		if (event.getRawSlot() < 0) return null;
 
 			return event.getRawSlot() < event.getView().getTopInventory().getSize() ? event.getView().getTopInventory() : event.getView().getBottomInventory();
+	}
+
+	private static final Class<?> CLASS_CONTAINER = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : NMSReflection.INSTANCE.getNmsClass("Container");
+	private static final Class<?> CLASS_CONTAINERS = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : NMSReflection.INSTANCE.getNmsClass("Containers");
+	private static final Constructor<?> CONSTRUCTOR_CHAT_MESSAGE = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : Reflection.getConstructor(NmsReflector.INSTANCE.getNmsClass("ChatMessage"), String.class, Object[].class);
+	private static final Constructor<?> CONSTRUCTOR_PACKET_PLAY_OUT_OPEN_WINDOW = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : Reflection.getConstructor(NmsReflector.INSTANCE.getNmsClass("PacketPlayOutOpenWindow"), int.class, MCVersion.isOlderThan(MCVersion.MC_1_14) ? String.class : CLASS_CONTAINERS, NMSReflection.getNMSClass("IChatBaseComponent"));
+	private static final Field FIELD_ACTIVE_CONTAINER = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : Reflection.getFieldIncludeParents(ENTITY_PLAYER, "activeContainer");
+	private static final Field FIELD_CONTAINER_WINDOW_ID = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : Reflection.getField(CLASS_CONTAINER, "windowId");
+	private static final Method METHOD_ENTITY_PLAYER_UPDATE_INVENTORY = (MCVersion.isOlderThan(MCVersion.MC_1_14)) ? null : Reflection.getMethod(ENTITY_PLAYER, "updateInventory", CLASS_CONTAINER);
+
+	public static void updateInventoryTitle(final @NotNull Player player, final @NotNull String newTitle)
+	{
+		if(MCVersion.isOlderThan(MCVersion.MC_1_14)) return;
+		InventoryView view = player.getOpenInventory();
+		Inventory topInv = view.getTopInventory();
+		if(topInv.getType() == InventoryType.CRAFTING) return;
+		try
+		{
+			Object entityPlayer = NMSReflection.getHandle(player);
+			if(entityPlayer == null || entityPlayer.getClass() != ENTITY_PLAYER) return; // Not a real player
+			Object title = CONSTRUCTOR_CHAT_MESSAGE.newInstance(newTitle, new Object[0]);
+			Object activeContainer = FIELD_ACTIVE_CONTAINER.get(entityPlayer);
+			Object windowId = FIELD_CONTAINER_WINDOW_ID.get(activeContainer);
+			String type = topInv.getType().name();
+			if(topInv.getType() == InventoryType.CHEST) type = "GENERIC_9X" + (topInv.getSize() / 9);
+			if(topInv.getType() == InventoryType.DISPENSER || topInv.getType() == InventoryType.DROPPER) type = "GENERIC_3X3";
+			if(topInv.getType() == InventoryType.BREWING) type = "BREWING_STAND";
+			Object packet = CONSTRUCTOR_PACKET_PLAY_OUT_OPEN_WINDOW.newInstance(windowId, MCVersion.isOlderThan(MCVersion.MC_1_14) ? type : Reflection.getField(CLASS_CONTAINERS, type).get(null), title);
+			SEND_PACKET.invoke(PLAYER_CONNECTION.get(entityPlayer), packet);
+			METHOD_ENTITY_PLAYER_UPDATE_INVENTORY.invoke(entityPlayer, activeContainer);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
