b73e696bf252e7d31ac6073358f236317963e95d
GeorgH93 <git@georgh93.eu>
2020-07-19 02:10:32 +0200

    Some cleanups

--- pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/NmsReflector.java
+++ pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/NmsReflector.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2019 GeorgH93
+ *   Copyright (C) 2020 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -91,7 +91,7 @@ public interface NmsReflector
 
 	@Nullable Object getNmsHandle(@NotNull Object obj);
 
-	public static @Nullable Object getHandle(@NotNull Object obj)
+	static @Nullable Object getHandle(@NotNull Object obj)
 	{
 		try
 		{
