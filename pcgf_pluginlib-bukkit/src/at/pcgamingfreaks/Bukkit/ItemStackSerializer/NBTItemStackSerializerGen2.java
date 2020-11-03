afbf48ca5ca529a82d9dc0e9589cf90b9285cef3
GeorgH93 <git@georgh93.eu>
2020-11-03 22:46:03 +0100

    Add support for MC 1.16.4

--- pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/ItemStackSerializer/NBTItemStackSerializerGen2.java
+++ pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/ItemStackSerializer/NBTItemStackSerializerGen2.java
@@ -286,6 +286,6 @@ public class NBTItemStackSerializerGen2 implements ItemStackSerializer
 
 	public static boolean isMCVersionCompatible()
 	{
-		return MCVersion.isNewerOrEqualThan(MCVersion.MC_1_7) && MCVersion.isOlderOrEqualThan(MCVersion.MC_NMS_1_16_R2);
+		return MCVersion.isNewerOrEqualThan(MCVersion.MC_1_7) && MCVersion.isOlderOrEqualThan(MCVersion.MC_NMS_1_16_R3);
 	}
 }
