32a690c602c0bc722fcf5313cc4c6c6ab77d7aa3
GeorgH93 <git@georgh93.eu>
2021-12-20 21:10:12 +0100

    Improve readability

--- pcgf_pluginlib-common/test/src/at/pcgamingfreaks/Message/MessageHoverEventTest.java
+++ pcgf_pluginlib-common/test/src/at/pcgamingfreaks/Message/MessageHoverEventTest.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2016 GeorgH93
+ *   Copyright (C) 2021 GeorgH93
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package at.pcgamingfreaks.Message;
@@ -53,27 +53,27 @@ public class MessageHoverEventTest
 	public void testConstructors()
 	{
 		MessageHoverEvent hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, "test1");
-		assertEquals(hoverEvent.getValue(), "test1");
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals("test1", hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, testObject);
-		assertEquals(hoverEvent.getValue(), testObject);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(testObject, hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, testMessage);
-		Assert.assertArrayEquals((Object[]) hoverEvent.getValue(), testComponentArray);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		Assert.assertArrayEquals(testComponentArray, (Object[]) hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, testComponentArray);
 		Assert.assertArrayEquals((Object[]) hoverEvent.getValue(), testComponentArray);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, testComponents);
-		assertEquals(hoverEvent.getValue(), testComponents);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(testComponents, hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 	}
 
 	@Test
 	public void testGetAction()
 	{
 		MessageHoverEvent hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, "123");
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 	}
 
 	@Test
@@ -81,20 +81,20 @@ public class MessageHoverEventTest
 	{
 		MessageHoverEvent hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, "123");
 		hoverEvent.setAction(MessageHoverEvent.HoverEventAction.SHOW_ITEM);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_ITEM);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_ITEM, hoverEvent.getAction());
 		hoverEvent.setAction(MessageHoverEvent.HoverEventAction.SHOW_ACHIEVEMENT);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_ACHIEVEMENT);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_ACHIEVEMENT, hoverEvent.getAction());
 		hoverEvent.setAction(MessageHoverEvent.HoverEventAction.SHOW_ENTITY);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_ENTITY);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_ENTITY, hoverEvent.getAction());
 		hoverEvent.setAction(MessageHoverEvent.HoverEventAction.valueOf("SHOW_ITEM"));
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_ITEM);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_ITEM, hoverEvent.getAction());
 	}
 
 	@Test
 	public void testGetValue()
 	{
 		MessageHoverEvent hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, "123");
-		assertEquals(hoverEvent.getValue(), "123");
+		assertEquals("123", hoverEvent.getValue());
 	}
 
 	@Test
@@ -102,19 +102,19 @@ public class MessageHoverEventTest
 	{
 		MessageHoverEvent hoverEvent = new MessageHoverEvent(MessageHoverEvent.HoverEventAction.SHOW_TEXT, "test1");
 		hoverEvent.setValue("test2");
-		assertEquals(hoverEvent.getValue(), "test2");
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals("test2", hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent.setValue(testObject);
-		assertEquals(hoverEvent.getValue(), testObject);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(testObject, hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent.setValue(testMessage);
 		Assert.assertArrayEquals((Object[]) hoverEvent.getValue(), testComponentArray);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent.setValue(testComponentArray);
 		Assert.assertArrayEquals((Object[]) hoverEvent.getValue(), testComponentArray);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 		hoverEvent.setValue(testComponents);
-		assertEquals(hoverEvent.getValue(), testComponents);
-		assertEquals(hoverEvent.getAction(), MessageHoverEvent.HoverEventAction.SHOW_TEXT);
+		assertEquals(testComponents, hoverEvent.getValue());
+		assertEquals(MessageHoverEvent.HoverEventAction.SHOW_TEXT, hoverEvent.getAction());
 	}
 }
