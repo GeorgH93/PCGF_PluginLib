455191ff386d8c9da3f59f61adf54f420a4fe333
GeorgH93 <git@georgh93.eu>
2022-03-21 21:49:59 +0100

    Cleanup code

--- pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/GUI/Navigation/DefaultNavigationButtonProducer.java
+++ pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/GUI/Navigation/DefaultNavigationButtonProducer.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2020 GeorgH93
+ *   Copyright (C) 2022 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ public class DefaultNavigationButtonProducer implements INavigationButtonProduce
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(ChatColor.RED + "Page " + page);
 		item.setItemMeta(meta);
-		return new GuiButton(item, (player, clickType, cursor) -> { gui.show(player, page); });
+		return new GuiButton(item, (player, clickType, cursor) -> gui.show(player, page));
 	}
 
 	@Override
@@ -48,20 +48,20 @@ public class DefaultNavigationButtonProducer implements INavigationButtonProduce
 		ItemMeta meta = item.getItemMeta();
 		meta.setDisplayName(ChatColor.DARK_GREEN + "Page " + page);
 		item.setItemMeta(meta);
-		return new GuiButton(item, (player, clickType, cursor) -> { gui.show(player, page); }, null);
+		return new GuiButton(item, (player, clickType, cursor) -> gui.show(player, page), null);
 	}
 
 	@Override
 	public @NotNull GuiButton produceNextButton(final @NotNull MultiPageGui gui, final int currentPage, final int pages)
 	{
 		ItemStack item = HeadUtils.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMmEzYjhmNjgxZGFhZDhiZjQzNmNhZThkYTNmZTgxMzFmNjJhMTYyYWI4MWFmNjM5YzNlMDY0NGFhNmFiYWMyZiJ9fX0=", ChatColor.GOLD + "Next Page", UUID.randomUUID());
-		return new GuiButton(item, (player, clickType, cursor) -> { gui.show(player, (currentPage < pages) ? currentPage + 1 : 1); });
+		return new GuiButton(item, (player, clickType, cursor) -> gui.show(player, (currentPage < pages) ? currentPage + 1 : 1));
 	}
 
 	@Override
 	public @NotNull GuiButton producePreviousButton(final @NotNull MultiPageGui gui, final int currentPage, final int pages)
 	{
 		ItemStack item = HeadUtils.fromBase64("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODY1MmUyYjkzNmNhODAyNmJkMjg2NTFkN2M5ZjI4MTlkMmU5MjM2OTc3MzRkMThkZmRiMTM1NTBmOGZkYWQ1ZiJ9fX0=", ChatColor.GOLD + "Previous Page", UUID.randomUUID());
-		return new GuiButton(item, (player, clickType, cursor) -> { gui.show(player, (currentPage > 1) ? currentPage - 1 : pages); });
+		return new GuiButton(item, (player, clickType, cursor) -> gui.show(player, (currentPage > 1) ? currentPage - 1 : pages));
 	}
 }
