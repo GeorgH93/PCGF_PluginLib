074c89f6c7b66fcbafffb7c6b7971f2b752d310f
GeorgH93 <git@georgh93.eu>
2022-05-30 20:55:28 +0200

    Swap arguments so that they are in the right order

--- pcgf_pluginlib-common/test/src/at/pcgamingfreaks/Message/MessageClickEventTest.java
+++ pcgf_pluginlib-common/test/src/at/pcgamingfreaks/Message/MessageClickEventTest.java
@@ -1,18 +1,18 @@
 /*
- * Copyright (C) 2016 GeorgH93
+ *   Copyright (C) 2022 GeorgH93
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package at.pcgamingfreaks.Message;
@@ -27,7 +27,7 @@ public class MessageClickEventTest
 	public void testGetAction()
 	{
 		MessageClickEvent clickEvent = new MessageClickEvent(MessageClickEvent.ClickEventAction.CHANGE_PAGE, "123");
-		assertEquals(clickEvent.getAction(), MessageClickEvent.ClickEventAction.CHANGE_PAGE);
+		assertEquals(MessageClickEvent.ClickEventAction.CHANGE_PAGE, clickEvent.getAction());
 	}
 
 	@Test
@@ -35,22 +35,22 @@ public class MessageClickEventTest
 	{
 		MessageClickEvent clickEvent = new MessageClickEvent(MessageClickEvent.ClickEventAction.CHANGE_PAGE, "123");
 		clickEvent.setAction(MessageClickEvent.ClickEventAction.OPEN_FILE);
-		assertEquals(clickEvent.getAction(), MessageClickEvent.ClickEventAction.OPEN_FILE);
+		assertEquals(MessageClickEvent.ClickEventAction.OPEN_FILE, clickEvent.getAction());
 		clickEvent.setAction(MessageClickEvent.ClickEventAction.OPEN_URL);
-		assertEquals(clickEvent.getAction(), MessageClickEvent.ClickEventAction.OPEN_URL);
+		assertEquals(MessageClickEvent.ClickEventAction.OPEN_URL, clickEvent.getAction());
 		clickEvent.setAction(MessageClickEvent.ClickEventAction.RUN_COMMAND);
-		assertEquals(clickEvent.getAction(), MessageClickEvent.ClickEventAction.RUN_COMMAND);
+		assertEquals(MessageClickEvent.ClickEventAction.RUN_COMMAND, clickEvent.getAction());
 		clickEvent.setAction(MessageClickEvent.ClickEventAction.SUGGEST_COMMAND);
-		assertEquals(clickEvent.getAction(), MessageClickEvent.ClickEventAction.SUGGEST_COMMAND);
+		assertEquals(MessageClickEvent.ClickEventAction.SUGGEST_COMMAND, clickEvent.getAction());
 		clickEvent.setAction(MessageClickEvent.ClickEventAction.valueOf("OPEN_URL"));
-		assertEquals(clickEvent.getAction(), MessageClickEvent.ClickEventAction.OPEN_URL);
+		assertEquals(MessageClickEvent.ClickEventAction.OPEN_URL, clickEvent.getAction());
 	}
 
 	@Test
 	public void testGetValue()
 	{
 		MessageClickEvent clickEvent = new MessageClickEvent(MessageClickEvent.ClickEventAction.SUGGEST_COMMAND, "123");
-		assertEquals(clickEvent.getValue(), "123");
+		assertEquals("123", clickEvent.getValue());
 	}
 
 	@Test
@@ -58,6 +58,6 @@ public class MessageClickEventTest
 	{
 		MessageClickEvent clickEvent = new MessageClickEvent(MessageClickEvent.ClickEventAction.SUGGEST_COMMAND, "123");
 		clickEvent.setValue("testValue");
-		assertEquals(clickEvent.getValue(), "testValue");
+		assertEquals("testValue", clickEvent.getValue());
 	}
 }
