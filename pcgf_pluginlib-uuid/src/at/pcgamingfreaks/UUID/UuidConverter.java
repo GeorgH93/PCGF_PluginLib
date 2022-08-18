f5f401551ebe9b08ec9c384f21415a27a731a01f
GeorgH93 <git@georgh93.eu>
2022-08-18 21:20:13 +0200

    Add getName to UuidConverter

--- pcgf_pluginlib-uuid/src/at/pcgamingfreaks/UUID/UuidConverter.java
+++ pcgf_pluginlib-uuid/src/at/pcgamingfreaks/UUID/UuidConverter.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2021 GeorgH93
+ *   Copyright (C) 2022 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -88,5 +88,10 @@ public class UuidConverter
 		UUID uuid = mojangUuidResolver.getUUID(name, null);
 		if(uuid != null || !offlineModeFallback) return uuid;
 		return getOfflineModeUUID(name);
+	}
+
+	public String getName(final @NotNull UUID uuid)
+	{
+		return mojangUuidResolver.getName(uuid);
 	}
 }
