d201913cb39d04b50af852c93007d35d3e58c34f
GeorgH93 <git@georgh93.eu>
2023-05-06 17:48:04 +0200

    Remove unused import

--- pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/Util/Utils.java
+++ pcgf_pluginlib-bukkit/src/at/pcgamingfreaks/Bukkit/Util/Utils.java
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2022 GeorgH93
+ *   Copyright (C) 2023 GeorgH93
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -37,7 +37,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import java.util.logging.Logger;
 
 public final class Utils extends at.pcgamingfreaks.Utils
